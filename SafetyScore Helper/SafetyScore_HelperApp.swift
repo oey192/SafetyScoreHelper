f24185b86407f59b4661f3d3913c59ddbb1e73cf
oey192 <oeystanger@gmail.com>
2021-10-06 15:12:21 -0700

    Initial Commit

---
+++ SafetyScore Helper/SafetyScore_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafetyScore_HelperApp.swift
+//  SafetyScore Helper
+//
+//  Created by Orion on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SafetyScore_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
